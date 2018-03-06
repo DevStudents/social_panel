dba58913d4f3090fa25dba6d1c9403fb5dce49b3
MilenaSchmidt <schmidtmilena@protonmail.com>
2018-03-06 14:40:24 +0100

    posting has been added

---
+++ app/Http/Controllers/PostsController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Post;
+use App\User;
+use Illuminate\Support\Facades\Auth;
+
+class PostsController extends Controller
+{
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validate($request,[
+            'post_content' => 'required|min:5',
+        ],[
+            'required' => 'You cannot add empty post silly...',
+            'min' => "C'mon your post must be at least :min characters long..."
+        ]);
+
+        Post::create([
+            'user_id' => Auth::id(),
+            'post_content' => $request->post_content,
+        ]);
+        return back();
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
---
+++ app/Post.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Post extends Model
+{
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id', 'post_content',
+    ];
+
+    protected $dates = ['deleted_at'];
+}

a4d09f6999a8ef6eadc5b548558c6f1886e621bc
MilenaSchmidt <schmidtmilena@protonmail.com>
2018-03-24 15:42:03 +0100

    Wall: scrolling script

--- resources/views/post/main.blade.php
+++ resources/views/post/main.blade.php
@@ -16,7 +16,42 @@
             </div>
         @endforelse
             {{ $posts->links() }}
-    </div>
+        </div>
+            <button class="fixed-btn">^</button>
         </div>
     </div>
+    <script>
+
+
+        window.addEventListener('scroll',function () {
+            if(window.pageYOffset > 700){
+            document.querySelector('.fixed-btn').style.display='block';
+            }
+            else{
+                document.querySelector('.fixed-btn').style.display='none';
+            }
+        });
+
+
+        var userScroll = false;
+        window.addEventListener('wheel',function () {
+            userScroll = true;
+            setTimeout(function () {
+            userScroll = false;
+            },1000/61);
+        });
+
+        window.addEventListener('DOMContentLoaded', function () {
+            document.querySelector('.fixed-btn').addEventListener('click',function () {
+               var postSroll = setInterval(function () {
+               window.scrollBy(0,((window.pageYOffset/20)*-1)-10);
+               if(window.pageYOffset === 0 || userScroll){
+               clearInterval(postSroll);
+               }
+               },1000/60);
+            });
+        })
+
+
+    </script>
 @endsection
